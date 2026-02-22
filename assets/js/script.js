689121f1174fcfd8e01523276cc0d88ad3541dfa
lucky23032002 <109616676+lucky23032002@users.noreply.github.com>
2026-02-22 21:41:11 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,9 +39,9 @@ $(document).ready(function () {
 
     // <!-- emailjs to mail contact form data -->
     $("#contact-form").submit(function (event) {
-        emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
+        emailjs.init("y8fd9n6BtHl-jta6_");
 
-        emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
+        emailjs.sendForm('service_sd88aqm', 'template_7nowbo2', '#contact-form')
             .then(function (response) {
                 console.log('SUCCESS!', response.status, response.text);
                 document.getElementById("contact-form").reset();
